dbcd87b55137fea1ad8bc176ecb6ec5b08dec411
Iqbal Erani <iqbalerani@gmail.com>
2023-02-17 18:13:25 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+        doloremque sed explicabo rem aliquam, debitis adipisci magni amet
+        laborum consectetur obcaecati minima quia impedit quo a ipsam dolore
+        excepturi aut!
+      </div>
+    </>
   );
 }
 
